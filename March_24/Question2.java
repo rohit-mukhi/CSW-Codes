77b265d9ca35d488a54ca95e438de28b763119c2
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-03-24 17:55:58 +0530

    Update Question2.java

--- March_24/Question2.java
+++ March_24/Question2.java
@@ -5,7 +5,19 @@ public class Question2 {
         str = str.concat("Programming");
         System.out.println(str);
         System.out.println(str.hashCode());
+        // It looks like you changed the String in the same memory location, but it happened in a new location while the original location was left but not deleted.
 
-        // It looks like you changed the String in the same memory location, but it happened in a new location while the opiginal location was left but not deleted.
+        // Below are some functions of string
+        System.out.println(str.length());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+        System.out.println(str.trim());
+        System.out.println(str.substring(0, 7));
+        System.out.println(str.replace(" Programming", "Script"));
+        String str2 = "Apple,Banana,Cherry";
+        String arr[] = str2.split(",");
+        for(String fruit : arr) {
+            System.out.println(fruit);
+        }
     }
 }
