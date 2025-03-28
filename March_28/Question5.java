c2a5d0ea65f298c5abc761a79b3196a30b026570
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-03-28 09:36:09 +0530

    Update Question5.java

--- March_28/Question5.java
+++ March_28/Question5.java
@@ -1,1 +1,18 @@
-hello
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Question5 {
+    public static void main(String[] args) {
+        String str1 = "Banana";
+        String str2 = "Banana";
+        // Checking lexicographically
+        System.out.println(str1.compareTo(str2));
+
+        List<String> list = Arrays.asList("Apple", "Banana", "Cherry");
+        Collections.sort(list);
+        // The sort method works because the toCompare() is part of String 
+        System.out.println(list.toString());
+    }    
+}
+
