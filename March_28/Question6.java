c27dda2db2e07b1660259b366dcf2dea47bfc9e0
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-03-28 09:59:48 +0530

    Update Question6.java

--- March_28/Question6.java
+++ March_28/Question6.java
@@ -1,5 +1,3 @@
-// Here is exampple of CharSequence interface.
-
 public class Question6 {    
     public static void main(String[] args) {
         CharSequence chs = "HPKILY";
@@ -7,5 +5,35 @@ public class Question6 {
         System.out.println("Length of String : " + chs.length());
         System.out.println("A subsequence is : " + chs.subSequence(1, 3));
         System.out.println("Original String : " + chs.toString());
+        System.out.println();
+
+        Fruits name = new Fruits("Strawberry");
+        System.out.println(name.length());
+        System.out.println(name.subSequence(1, 3));
+        System.out.println(name.charAt(2));
+
+
+    }
+}
+
+class Fruits implements CharSequence {
+    private String fruitName;
+    Fruits(String fruitName) {
+        this.fruitName = fruitName;
+    }
+
+    @Override
+    public int length() {
+        return fruitName.length();
+    }
+
+    @Override
+    public char charAt(int i) {
+        return fruitName.charAt(i);
+    }
+
+    @Override
+    public String subSequence(int s, int e) {
+        return fruitName.substring(s, e);
     }
 }
