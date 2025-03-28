cfad96b9c9a0f8addc9a49c037e95d08b9aba995
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-03-28 09:00:53 +0530

    Update Question2.java

--- March_28/Question2.java
+++ March_28/Question2.java
@@ -7,5 +7,13 @@ public class Question2 {
         System.out.println(str1 == str2);
         System.out.println(str2 == "Rohit");
         System.out.println(str2 == str3);
+
+        String str4 = new String("Joker").intern();
+        String str5 = "Joker";
+        String str6 = new String("Joker").intern();
+
+        System.out.println(str4 == str5);
+        System.out.println(str4 == str6);
+        System.out.println(str5 == str6);
     }    
 }
