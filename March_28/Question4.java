fd85098d8e3641c147cd90aebfe29f2f3b4e36e9
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-03-28 09:17:20 +0530

    Create Question4.java

---
+++ March_28/Question4.java
@@ -0,0 +1,16 @@
+// We can see all the locations are different
+
+public class Question4 {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        System.out.println(System.identityHashCode(str1));
+        str1 = str1.concat(" World");
+        System.out.println(System.identityHashCode(str1));
+
+        String str2 = str1.concat(" World");
+        System.out.println(System.identityHashCode(str2));
+
+        String str3 = "Hello World";
+        System.out.println(System.identityHashCode(str3));
+    }
+}
