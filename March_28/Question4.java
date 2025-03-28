dedf592c107c887234edce1aa90c6af26df50f29
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-03-28 09:22:20 +0530

    Update Question4.java

--- March_28/Question4.java
+++ March_28/Question4.java
@@ -3,14 +3,16 @@
 public class Question4 {
     public static void main(String[] args) {
         String str1 = "Hello";
+        String str2 = str1.concat(" World");
         System.out.println(System.identityHashCode(str1));
         str1 = str1.concat(" World");
         System.out.println(System.identityHashCode(str1));
-
-        String str2 = str1.concat(" World");
         System.out.println(System.identityHashCode(str2));
 
         String str3 = "Hello World";
         System.out.println(System.identityHashCode(str3));
+
+        String str4 = "Hello" + " World";
+        System.out.println(System.identityHashCode(str4));
     }
 }
