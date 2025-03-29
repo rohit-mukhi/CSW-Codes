fc1c5f6bb39e5ebd0a78ba93212de6cfff00bef8
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-03-29 15:37:07 +0530

    Create Question3.java

---
+++ March_29/Question3.java
@@ -0,0 +1,8 @@
+public class Question3 {
+    public static void main(String[] args) throws Exception {
+        Appendable obj = new StringBuilder();
+        obj.append("Hi Buck");
+        obj.append('!');
+        System.out.println(obj);
+    }
+}
