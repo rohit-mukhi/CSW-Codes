4f65ee1b836f2edac6856c34b549958de0130998
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-03-29 14:40:21 +0530

    Update Question3.java

--- March_28/Question3.java
+++ March_28/Question3.java
@@ -5,5 +5,20 @@ public class Question3 {
 
         System.out.println(str1 == str2);
         System.out.println(str1.equals(str2));
+
+        String str3 = "Hello";
+        String str4 = "hello";
+        System.out.println(str3.equalsIgnoreCase(str4);
+        System.out.println(str3.firstIndexOf('H'));
+        System.out.println(str4.lastIndexOf('o'));
+        System.out.println(str3.startsWith("He"));
+        System.out.println(str4.endsWith("lo"));
+        System.out.println(str4.toCharArray());
+
+        String str5 = "18";
+        System.out.println(Integer.parseInt(str5));
+
+        int val = 143;
+        System.out.println(String.valueOf(val));
     }
 }
