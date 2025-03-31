d25b999b11de2b6e8dfc30e767c8a379abb499c4
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-03-31 16:15:49 +0530

    Create Question4.java

---
+++ Assignment_5/Question4.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Question4 {
+    public static void main(String[] args) {
+          Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a word : ");
+        String word = scan.next();
+        StringBuffer obj = new StringBuffer(word);
+        char ch = 'y';
+        String str;
+        int s, e;
+        while(ch == 'y') {
+            System.out.print("Enter operation (1-append at, 2-delete, 3-replace at, 4-concat at end, 5-display) : ");
+            int op = scan.nextInt();
+            switch(op) {
+                case 1: System.out.print("Enter word to append : ");
+                        str = scan.next();
+                        char chs[] = str.toCharArray();
+                        System.out.println("Enter start index : ");
+                        s = scan.nextInt();
+                        obj.append(chs, s, chs.length);
+                        break;
+                case 2: System.out.print("Enter start and end indices for deletion : ");
+                        s = scan.nextInt();
+                        e = scan.nextInt();
+                        obj.delete(s-1, e-1);
+                        break;
+                case 3: System.out.print("Enter start and end indices and word to replace : ");
+                        s = scan.nextInt();
+                        e = scan.nextInt();
+                        str = scan.next();
+                        obj.replace(s-1, s-1, str);
+                        break;
+                case 4: System.out.println("Enter word to concat at end : ");
+                        str = scan.next();
+                        obj.append(str);
+                        break;
+                case 5: System.out.print(obj.toString());
+                        System.out.println();
+                        break;
+                default: System.out.print("Invalid input!");
+                        break;
+            }
+            System.out.print("Do you want to continue (y/n) : ");
+            ch = scan.next().charAt(0);
+            ch = Character.toLowerCase(ch);
+        }
+        System.out.println();
+        System.out.println("See you again!");
+        scan.close();
+       
+    }
+}
