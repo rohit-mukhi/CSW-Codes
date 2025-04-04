3e4e50e3e1c746069a1575c6337cf7d183f446a7
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-04-04 10:26:23 +0530

    Create Question1.java

---
+++ Assignment_6/Question1.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Question8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String filename = "diary.txt";
+        File file = new File(filename);
+        if(file.exists()) {
+            System.out.println("File exists");
+        } else {
+            System.out.println("Creating new file!");
+        }
+        System.out.print("Enter your diary entry : ");
+        String diaryEntry = scanner.nextLine();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-mm-dd HH:mm:ss");
+        String currentDateTime = LocalDateTime.now().format(formatter);
+
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(filename));
+            bw.write(diaryEntry);
+            bw.newLine();
+            bw.write(currentDateTime);
+            bw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        scanner.close();
+    }
+}
