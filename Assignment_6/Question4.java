f7e89db4a63a94504079dff440c2eab5ad966ac4
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-04-05 14:16:41 +0530

    Create Question4.java

---
+++ Assignment_6/Question4.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Question4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter directory path : ");
+        File file = new File(scan.nextLine());
+
+        if(file.exists() && file.isDirectory()) {
+            File files[] = file.listFiles();
+            if(files != null) {
+                for(File f : files) {
+                    System.out.println(f.getName());
+                }
+            } else {
+                System.out.println("No files in the directory");
+            }
+        } else {
+            System.out.println("Invalid directory path");
+        }
+        scan.close();
+    }
+}
+
