f253b90ecc297a299db868c61fb00faed0f072d4
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-05-02 09:57:27 +0530

    Create AdjacencyList.java

---
+++ Data Structures in CSW/AdjacencyList.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class GraphList {
+    int vertices;
+    List <Integer> adjList[];
+
+    public GraphList(int v) {
+        vertices = v;
+        adjList = new ArrayList[v];
+        for(int i=0; i<v; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+    }
+
+    void addEdge(int u, int v) {
+        adjList[u].add(v);
+        adjList[v].add(u);
+    }
+
+    void printGraph() {
+        for(int i=0; i<vertices; i++) {
+            System.out.print(i + " -> ");
+            for(int node : adjList[i])
+                System.out.print(node + " ");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        GraphList list = new GraphList(5);
+        list.addEdge(0, 1);
+        list.addEdge(0, 2);
+        list.addEdge(2, 3);
+        list.addEdge(1, 4);
+        list.addEdge(3, 4);
+        list.addEdge(1, 2);
+        list.printGraph();
+    }
+}
