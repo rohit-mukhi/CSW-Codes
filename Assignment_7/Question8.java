1139817d9c25af33dfd65431689e0c1aafd64163
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-05-09 08:45:50 +0530

    Update Question8.java

--- Assignment_7/Question8.java
+++ Assignment_7/Question8.java
@@ -1,1 +1,12 @@
 // Assignment questions begin from here
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class Question2 {
+    public static void main(String[] args) {
+        LocalDateTime date = LocalDateTime.of(2025, 5, 9, 8, 44, 20);
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd:mm:yyyy hh:mm:ss");
+        System.out.println("Date and time: " + date.format(format));
+    }
+}
