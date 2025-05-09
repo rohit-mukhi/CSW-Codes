d323413a5f4c8020ca196bf5318528368bea3897
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-05-09 09:12:07 +0530

    Update Question_11.java

--- Assignment_7/Question_11.java
+++ Assignment_7/Question_11.java
@@ -6,8 +6,11 @@ public class Question5 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
-        LocalDate date1 = LocalDate.of(2025, 5, 9);
-        LocalDate date2 = LocalDate.of(2025, 6, 30);
+        System.out.print("Enter date 1 (yyyy-mm-dd): ");
+        LocalDate date1 = LocalDate.parse(scan.next());
+
+        System.out.print("Enter date 2 (yyyy-mm-dd): ");
+        LocalDate date2 = LocalDate.parse(scan.next());
 
         long days = ChronoUnit.DAYS.between(date1, date2);
 
