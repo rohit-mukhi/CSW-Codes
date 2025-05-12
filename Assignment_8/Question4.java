0c0a31d05f3a4e1bed75989950b20535601a6dd4
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-05-12 17:50:50 +0530

    Update Question4.java

--- Assignment_8/Question4.java
+++ Assignment_8/Question4.java
@@ -1,1 +1,23 @@
-bf
+// Here we have not handled concurrency
+
+public class Question4 {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                for(int i=0; i<=3; i++) 
+                    System.out.println("Thread running: " + Thread.currentThread().getName() + " count: " + i);
+            }
+        });        
+        Thread t2 = new Thread(new Runnable() {
+           public void run() {
+                for(int i=0; i<=3; i++)
+                    System.out.println("Thread running: " + Thread.currentThread().getName() + " count: " + i);
+           } 
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
+
+
